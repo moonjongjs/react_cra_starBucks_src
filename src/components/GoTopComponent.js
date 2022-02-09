47d2779b6ca2bfae20f6bb592b664f154b025697
moonjongjs <moonseonjong@naver.com>
2022-02-09 12:20:11 +0900

    2022-02-09  PUBLIC_URL 경로와 웨사이트 수정

--- src/components/GoTopComponent.js
+++ src/components/GoTopComponent.js
@@ -5,8 +5,8 @@ class GoTopComponent extends Component {
     return (
       <React.Fragment>
           <div className="go-top">
-              <a href="#" className="go-top-btn">
-                  <img src="./images/btn_prom_up.png" alt="go Top 버튼"/>
+              <a href="#" className="go-top-btn">                
+                  <img src={"." + process.env.PUBLIC_URL + "/images/btn_prom_up.png"} alt="go Top 버튼"/>
               </a>
           </div>
       </React.Fragment>
